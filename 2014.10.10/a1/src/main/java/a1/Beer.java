e673ec24d202c1641171f51252e3e436694d2e5f
kimdonghyuk <kimadsle@naver.com>
2014-10-15 19:55:39 +0900

    Revert "2014.10.10. Spring Example"
    This reverts commit 5c2fd7345ac60d42dcd6106cb2f54a0b9c55c61a.

--- 2014.10.10/a1/src/main/java/a1/Beer.java
+++
@@ -1,9 +0,0 @@
-package a1;
-
-import org.springframework.stereotype.Component;
-
-//Componet : version2
-@Component
-public class Beer implements Snack {
-
-}
--- 2014.10.10/web/src/main/java/a1/Pizza.java
+++
@@ -1,8 +0,0 @@
-package a1;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Pizza implements Snack {
-
-}
